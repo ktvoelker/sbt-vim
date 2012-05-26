fa069cdac70cee3ec669c803103d2d2c5ae11e48
Karl Voelker <ktvoelker@gmail.com>
2012-05-26 14:16:34 -0700

    Made some classes to tidy things up

--- display.py
+++ display.py
@@ -6,73 +6,77 @@ import sys
 from log import log
 import sbt
 
-next_color_pair = 1
-def alloc_color_pair(fg, bg):
-  global next_color_pair
-  pair = next_color_pair
-  next_color_pair += 1
-  curses.init_pair(pair, fg, bg)
-  return curses.color_pair(pair)
+class Display(object):
 
-def handle_signal(sig, stack):
-  cleanup()
-  if sig == signal.SIGTERM:
-    sys.exit(0)
-  else:
-    sys.exit(1)
+  def __init__(self, sbt):
+    log("init display", sbt=sbt)
+    self.sbt = sbt
+    self.next_color_pair = 1
 
-def init_title_bar():
-  title_color = alloc_color_pair(curses.COLOR_WHITE, curses.COLOR_BLUE)
-  title_bar.bkgdset(' ', title_color)
-  title_bar.clear()
-  (h, w) = title_bar.getmaxyx()
-  title_bar.addstr(" csbt 0.0.1    ")
-  log(pi=sbt.project_info)
-  title_bar.addstr("%s" % sbt.project_info['name'], curses.A_BOLD)
-  ver_str = "sbt %s    scala %s " % (
-      sbt.project_info['sbt_ver'], sbt.project_info['scala_ver'])
-  title_bar.insstr(0, w - len(ver_str), ver_str)
+  def handle_signal(self, sig, stack):
+    if sig == signal.SIGTERM:
+      sys.exit(0)
+    else:
+      sys.exit(1)
 
-def init():
-  global root, title_bar, status_bar, content
-  # Get the whole terminal as a Window object
-  root = curses.initscr()
-  curses.savetty()
-  # Clean up the terminal if we get killed
-  signal.signal(signal.SIGTERM, handle_signal)
-  # Enable colors
-  curses.start_color()
-  # Get one character at a time
-  curses.cbreak()
-  curses.noecho()
-  # Hide the cursor
-  curses.curs_set(0)
-  # Get the size of the terminal
-  (h, w) = root.getmaxyx()
-  # Make windows for the top and bottom bars
-  title_bar = root.subwin(1, w, 0, 0)
-  init_title_bar()
-  status_bar = root.subwin(1, w, h - 1, 0)
-  status_color = alloc_color_pair(curses.COLOR_BLACK, curses.COLOR_GREEN)
-  status_bar.bkgdset(' ', status_color)
-  status_bar.clear()
-  status_bar.insstr(0, 1, "0 errors")
-  # Make a window for the main content area
-  content = root.subpad(h - 2, w, 1, 0)
-  main_color = alloc_color_pair(curses.COLOR_BLACK, curses.COLOR_WHITE)
-  content.bkgdset(' ', main_color)
-  content.clear()
+  def __enter__(self):
+    # Get the whole terminal as a Window object
+    self.root = curses.initscr()
+    curses.savetty()
+    # Clean up the terminal if we get killed
+    signal.signal(signal.SIGTERM, lambda(sig, stack): self.handle_signal(sig, stack))
+    # Enable colors
+    curses.start_color()
+    # Get one character at a time
+    curses.cbreak()
+    curses.noecho()
+    # Hide the cursor
+    curses.curs_set(0)
+    # Get the size of the terminal
+    (h, w) = self.root.getmaxyx()
+    # Make windows for the top and bottom bars
+    self.title_bar = self.root.subwin(1, w, 0, 0)
+    self.init_title_bar()
+    self.status_bar = self.root.subwin(1, w, h - 1, 0)
+    status_color = self.alloc_color_pair(curses.COLOR_BLACK, curses.COLOR_GREEN)
+    self.status_bar.bkgdset(' ', status_color)
+    self.status_bar.clear()
+    self.status_bar.insstr(0, 1, "0 errors")
+    # Make a window for the main content area
+    self.content = self.root.subpad(h - 2, w, 1, 0)
+    main_color = self.alloc_color_pair(curses.COLOR_BLACK, curses.COLOR_WHITE)
+    self.content.bkgdset(' ', main_color)
+    self.content.clear()
+    return self
 
-def run(handle_key):
-  try:
-    ch = root.getch()
-    while ch >= 0:
-      handle_key(ch, curses.keyname(ch))
-      ch = root.getch()
-  except KeyboardInterrupt:
-    pass
+  def __exit__(self, type, value, traceback):
+    curses.resetty()
+    curses.endwin()
 
-def cleanup():
-  curses.resetty()
-  curses.endwin()
+  def alloc_color_pair(self, fg, bg):
+    pair = self.next_color_pair
+    self.next_color_pair += 1
+    curses.init_pair(pair, fg, bg)
+    return curses.color_pair(pair)
+
+  def init_title_bar(self):
+    title_color = self.alloc_color_pair(curses.COLOR_WHITE, curses.COLOR_BLUE)
+    self.title_bar.bkgdset(' ', title_color)
+    self.title_bar.clear()
+    (h, w) = self.title_bar.getmaxyx()
+    self.title_bar.addstr(" csbt 0.0.1    ")
+    log(pi=self.sbt.project_info)
+    self.title_bar.addstr("%s" % self.sbt.project_info['name'], curses.A_BOLD)
+    ver_str = "sbt %s    scala %s " % (
+        self.sbt.project_info['sbt_ver'], self.sbt.project_info['scala_ver'])
+    self.title_bar.insstr(0, w - len(ver_str), ver_str)
+
+  def run(self, handle_key):
+    try:
+      ch = self.root.getch()
+      while ch >= 0:
+        handle_key(ch, curses.keyname(ch))
+        ch = self.root.getch()
+    except KeyboardInterrupt:
+      pass
 
--- sbt.py
+++ sbt.py
@@ -5,77 +5,100 @@ import uuid
 
 from log import log
 
-proc = None
-project_info = None
+class CompileError(object):
 
-def command(cmd):
-  marker = str(uuid.uuid4())
-  log("command %s" % cmd)
-  log("marker %s" % marker)
-  proc.stdin.write(cmd)
-  proc.stdin.write("\n")
-  proc.stdin.write("eval print(\"%s\\n\")\n" % marker)
-  lines = []
-  line = proc.stdout.readline()
-  log("discard line %s" % line)
-  line = proc.stdout.readline()
-  while line:
-    log("got line %s" % line)
-    if line.strip() == marker:
-      log("found marker; read one more line")
-      line = proc.stdout.readline()
-      log("discard line %s" % line)
-      break
-    lines.append(line)
+  def __init__(self, file=None, line=None, col=None, short=None, long=None, code=None):
+    self.file = file
+    self.line = line
+    self.col = col
+    self.short = short
+    self.long = long
+    self.code = code
+
+def strip_error_tag(line):
+  return line[len("[error] "):]
+
+def line_is_error(line):
+  return line.startswith("[error] ")
+
+class Sbt(object):
+
+  def __init__(self):
+    pass
+
+  def __enter__(self):
+    self.proc = subprocess.Popen(
+        ["sbt", "-Dsbt.log.noformat=true"],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        close_fds=True)
+    self.project_info = self.get_project_info()
+    return self
+
+  def __exit__(self, type, value, traceback):
+    self.proc.stdin.close()
+    self.proc.wait()
+
+  def command(self, cmd):
+    proc = self.proc
+    marker = str(uuid.uuid4())
+    log("command %s" % cmd)
+    log("marker %s" % marker)
+    proc.stdin.write(cmd)
+    proc.stdin.write("\n")
+    proc.stdin.write("eval print(\"%s\\n\")\n" % marker)
+    lines = []
     line = proc.stdout.readline()
-  log("done reading")
-  return lines
+    log("discard line %s" % line)
+    line = proc.stdout.readline()
+    while line:
+      log("got line %s" % line)
+      if line.strip() == marker:
+        log("found marker; read one more line")
+        line = proc.stdout.readline()
+        log("discard line %s" % line)
+        break
+      lines.append(line)
+      line = proc.stdout.readline()
+    log("done reading")
+    return lines
 
-def get_project_info():
-  name_line = proc.stdout.readline()
-  log("name line %s" % name_line)
-  lines = command("about")
-  sbt_ver_line = lines[0]
-  scala_ver_line = lines[2]
-  ret = dict()
-  name_mo = re.match(
-      r"\[info\] Set current project to (.+) \(in build file:", name_line)
-  log(name_mo=name_mo)
-  if name_mo:
-    ret['name'] = name_mo.group(1)
-  else:
-    ret['name'] = None
-  sbt_ver_mo = re.match(
-      r"\[info\] This is sbt ([\d\.]+)", sbt_ver_line)
-  if sbt_ver_mo:
-    ret['sbt_ver'] = sbt_ver_mo.group(1)
-  else:
-    ret['sbt_ver'] = None
-  scala_ver_mo = re.match(
-      r"\[info\] The current project is built against Scala ([\d\.]+)",
-      scala_ver_line)
-  if scala_ver_mo:
-    ret['scala_ver'] = scala_ver_mo.group(1)
-  else:
-    ret['scala_ver'] = None
-  return ret
+  def get_project_info(self):
+    name_line = self.proc.stdout.readline()
+    log("name line %s" % name_line)
+    lines = self.command("about")
+    sbt_ver_line = lines[0]
+    scala_ver_line = lines[2]
+    ret = dict()
+    name_mo = re.match(
+        r"\[info\] Set current project to (.+) \(in build file:", name_line)
+    log(name_mo=name_mo)
+    if name_mo:
+      ret['name'] = name_mo.group(1)
+    else:
+      ret['name'] = None
+    sbt_ver_mo = re.match(
+        r"\[info\] This is sbt ([\d\.]+)", sbt_ver_line)
+    if sbt_ver_mo:
+      ret['sbt_ver'] = sbt_ver_mo.group(1)
+    else:
+      ret['sbt_ver'] = None
+    scala_ver_mo = re.match(
+        r"\[info\] The current project is built against Scala ([\d\.]+)",
+        scala_ver_line)
+    if scala_ver_mo:
+      ret['scala_ver'] = scala_ver_mo.group(1)
+    else:
+      ret['scala_ver'] = None
+    return ret
 
-def compile():
-  pass
+  def compile(self):
+    lines = map(strip_error_tag, filter(line_is_error, self.command("compile")))
+    log("ERRORS:")
+    for line in lines:
+      log(line)
+    log("END.")
 
-def test():
-  pass
-
-def init():
-  global proc, project_info
-  proc = subprocess.Popen(
-      ["sbt", "-Dsbt.log.noformat=true"],
-      stdin=subprocess.PIPE,
-      stdout=subprocess.PIPE,
-      close_fds=True)
-  project_info = get_project_info()
-
-def cleanup():
-  proc.stdin.close()
-  proc.wait()
+  def test(self):
+    pass
 
