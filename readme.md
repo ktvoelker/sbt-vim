51d268fc03fe87066b143a5c2f8fd133b941c507
Karl Voelker <ktvoelker@gmail.com>
2012-05-27 00:12:12 -0700

    Completely changed directions

--- readme.md
+++ readme.md
@@ -1,12 +1,25 @@
 
-CSBT
-====
+SBT-Vim Bridge
+==============
 
-This is CSBT, an interactive [`sbt`](https://github.com/harrah/xsbt/wiki) wrapper.
-The current goals are:
+This is a Vim plugin that allows [sbt](https://github.com/harrah/xsbt/wiki) to
+be used from within [Vim](http://www.vim.org/).
 
-1. Show one error message at a time, starting with the first
-2. Enabling jumping to an error in `vim`
+Usage:
 
-So far, #1 has been implemented and #2 has not.
+    :pyfile sbt-vim.py
+    :py sbt_compile()
+
+This puts you into Vim's
+[quickfix](http://vimdoc.sourceforge.net/htmldoc/quickfix.html) mode, where you
+can jump between compilation errors with `:cn` and `:cp`.
+
+The first time you compile, an interactive sbt session will be created in the
+background. This can be slow, but subsequent compiles will be faster.
+
+To-Do:
+
+1. A vim script that adds nicer bindings
+2. Proper vim plugin packaging
+3. Support for `sbt test`
 
---
+++ sbt-vim.py
@@ -0,0 +1,86 @@
+
+import re
+import subprocess
+import uuid
+import vim
+
+class SBT(object):
+
+  def __init__(self):
+    self.buffer = None
+    self.bufnum = None
+    self.proc = subprocess.Popen(
+        ["sbt", "-Dsbt.log.noformat=true"],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        close_fds=True)
+
+  def _init_buffer(self):
+    if not self.buffer:
+      vim.command("enew")
+      for i in xrange(0, len(vim.buffers)):
+        if vim.buffers[i] == vim.current.buffer:
+          self.bufnum = i + 1
+          break
+      if self.bufnum is None:
+        vim.command("bdelete")
+        raise RuntimeError("Couldn't find buffer in buffers array.")
+      self.buffer = vim.current.buffer
+      vim.command("setlocal buftype=nofile")
+      vim.command("setlocal bufhidden=hide")
+      vim.command("setlocal noswapfile")
+      vim.command("setlocal nobuflisted")
+      # TODO go back to the last buffer that was open
+      vim.command("bprevious")
+
+  def close(self):
+    if self.bufnum is not None:
+      vim.command("bdelete " + self.bufnum)
+    self.proc.stdin.close()
+    self.proc.wait()
+
+  def command(self, cmd):
+    to = self.proc.stdin
+    fr = self.proc.stdout
+    marker = str(uuid.uuid4())
+    to.write(cmd)
+    to.write("\n")
+    to.write("eval print(\"%s\\n\")\n" % marker)
+    fr.readline()
+    line = fr.readline()
+    while line:
+      if line.strip() == marker:
+        fr.readline()
+        break
+      yield line
+      line = fr.readline()
+
+  ERROR_TAG = "[error] /"
+  ERROR_TAG_LEN = len(ERROR_TAG) - 1
+
+  def _filter_errors(self, lines):
+    for line in lines:
+      if line.startswith(SBT.ERROR_TAG):
+        yield line[SBT.ERROR_TAG_LEN:]
+
+  def compile(self):
+    self._init_buffer()
+    del self.buffer[:]
+    lines = list(self._filter_errors(self.command("compile")))
+    if len(lines) > 0:
+      self.buffer[0] = lines[0]
+      self.buffer.append(lines[1:])
+      vim.command("cbuffer %d" % self.bufnum)
+
+sbt = None
+
+def sbt_init():
+  global sbt
+  if not sbt:
+    sbt = SBT()
+
+def sbt_compile():
+  global sbt
+  sbt_init()
+  sbt.compile()
+
